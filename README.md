27bdea32f7d334cbf29c26745e5f82bbcc510da1
lipeng <flyingsheep.lp@gmail.com>
2017-03-23 17:21:30 +0800

    add lambda_cps

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-#PyCps
+##PyCps##
+Convert lambda-calculus to CPS form implemented by python
+
+###Example###
+*python>* from cps import *
+
+*python>* docps('(lambda (x) (lambda (y) (+ x y)))')
+
+*python>* docps('(lambda (x) (+ x 2))')
+
+*python>* docps('((lambda (x) (+ x 2)) 3)')
--- cps.py
+++ cps.py
@@ -36,58 +36,100 @@ def atom(token):
         except ValueError:
             return Symbol(token)
 
-def apply_cps(s):
-    r = []
-    param = ['k']
-    body = [param[0],s]
-    r.append('lambda')
-    r.append(param)
-    r.append(body)
-    return r
+def rator(m):
+    print(m)
+    cps_trans(m)
+def apply_cps(rator,rand,k):
+    print("in apply cps rator:" + str(rator) + "  rand: "+ str(rand))
+    ab = ['k']
+    ae = []
+    if(rator[0] != 'lambda'):
+        ae.append(rator)
+        for r in rand:
+            ae.append(r)
+        return ae
 
-def lambda_cps(p,b):
-    r = []
-    param = ['K']
-    r.append('lambda')
-    r.append(param)
-    r.append(param[0])
+    print(cps_trans(rator,lambda m:m))
 
-    
-    pass
+    return cps_trans(rator,lambda m:cps_trans(rand,lambda n: m.append(n)))
 
-def symbol_cps(s):
-    r = []
-    param = ['K']
-    body = [param[0],s]
-    r.append('lambda')
-    r.append(param)
-    r.append(body)
-    return r
 
-def cps_trans(ast):
-    cps_r = []
+
+def con_param(param):
+    pl = []
+    for p in param:
+        pl.append(p)
+    pl.append('k')
+    return pl
+
+def con_body(body,k):
+    bs = ['k']
+    be = []
+    if(body[0] != 'lambda'):
+        for b in body:
+            be.append(b);
+        bs.append(be)
+    else:
+        (_,ip,ib) = body
+
+        t = lambda_cps(ip,ib,lambda l:l)
+
+    return bs
+
+def lambda_cps(p,b,k):
+
+    return k(['lambda',p,cps_trans(b,lambda m:m)])
+
+
+def cps_trans(ast,k):
+    #print("trans ast: " + str(ast))
     if not isinstance(ast,List):
-        cps_r.extend(symbol_cps(ast))
+        return k(ast)
     elif ast[0] == 'lambda':
         (_, param, body) = ast
-        lambda_cps(param,body)
-        pass
+        return lambda_cps(param,body,k)
     else:
-        cps_r.extend(apply_cps(ast))
-    return cps_r
+        rator = ast[0]
+        rand = []
+        for i in range(1,len(ast)):
+            rand.append(ast[i])
+
+        if(len(rand) == 1):
+            rand = rand[0]
+
+        t = apply_cps(rator,rand,k)
+        print("apply_cps res:" + str(t))
+
+        return t
+
+
+def cps(e):
+    return cps_trans(e,lambda m:m)
 
 def unparse_ast(s):
     for i in s:
         if isinstance(i,str):
-            sys.stdout.write(i+" ");
+            sys.stdout.write(i);
+
         elif isinstance(i,list):
             sys.stdout.write('(')
             unparse_ast(i)
             sys.stdout.write(')')
+ 
         else:
-            sys.stdout.write(str(i)+' ')
+            sys.stdout.write(str(i))
+
+        if(s.index(i) == len(s) - 1):
+            continue
+        sys.stdout.write(' ')
 
 def cps_out(cps_r):
-    sys.stdout.write('(')
-    unparse_ast(cps_r)
-    print(')\n')
+    if(isinstance(cps_r,Symbol) | isinstance(cps_r,Number)):
+        print(cps_r)
+    else:
+        sys.stdout.write('(')
+        unparse_ast(cps_r)
+        print(')')
+
+def docps(e):
+    cps_out(cps(parse(e)))
